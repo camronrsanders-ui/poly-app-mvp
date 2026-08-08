0548b1844b0d2b3b20e67f1bbb2d3a9d3a77fec4
camronrsanders-ui <camron.r.sanders@gmail.com>
2026-08-08 15:05:14 -0400

    Route matching through trusted Cloud Function

--- lib/services/connection_service.dart
+++ lib/services/connection_service.dart
@@ -1,46 +1,28 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:cloud_functions/cloud_functions.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 
 class ConnectionService {
-  ConnectionService({FirebaseFirestore? firestore, FirebaseAuth? auth})
-      : _firestore = firestore ?? FirebaseFirestore.instance,
-        _auth = auth ?? FirebaseAuth.instance;
+  ConnectionService({
+    FirebaseFirestore? firestore,
+    FirebaseAuth? auth,
+    FirebaseFunctions? functions,
+  })  : _firestore = firestore ?? FirebaseFirestore.instance,
+        _auth = auth ?? FirebaseAuth.instance,
+        _functions = functions ?? FirebaseFunctions.instance;
 
   final FirebaseFirestore _firestore;
   final FirebaseAuth _auth;
-
-  String _likeId(String fromUid, String toUid) => '${fromUid}_$toUid';
-
-  String _matchId(String a, String b) {
-    final pair = [a, b]..sort();
-    return '${pair[0]}_${pair[1]}';
-  }
+  final FirebaseFunctions _functions;
 
   Future<bool> likeUser(String toUid) async {
     final fromUid = _auth.currentUser?.uid;
     if (fromUid == null) throw StateError('No signed-in user.');
     if (fromUid == toUid) throw ArgumentError('You cannot like yourself.');
 
-    final likeRef = _firestore.collection('likes').doc(_likeId(fromUid, toUid));
-    final reciprocalRef = _firestore.collection('likes').doc(_likeId(toUid, fromUid));
-
-    await likeRef.set({
-      'fromUid': fromUid,
-      'toUid': toUid,
-      'createdAt': FieldValue.serverTimestamp(),
-    });
-
-    final reciprocal = await reciprocalRef.get();
-    if (!reciprocal.exists) return false;
-
-    final pair = [fromUid, toUid]..sort();
-    await _firestore.collection('matches').doc(_matchId(fromUid, toUid)).set({
-      'userAUid': pair[0],
-      'userBUid': pair[1],
-      'createdAt': FieldValue.serverTimestamp(),
-      'active': true,
-    }, SetOptions(merge: true));
-    return true;
+    final callable = _functions.httpsCallable('likeUser');
+    final result = await callable.call<Map<String, dynamic>>({'toUid': toUid});
+    return result.data['matched'] == true;
   }
 
   Stream<QuerySnapshot<Map<String, dynamic>>> watchMatches() {
