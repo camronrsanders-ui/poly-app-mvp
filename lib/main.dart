9a547c9a7cbe9b35bd6337004a207b299ecd05c3
camronrsanders-ui <camron.r.sanders@gmail.com>
2026-08-08 18:11:39 -0400

    Initialize Firebase App Check

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
+import 'package:firebase_app_check/firebase_app_check.dart';
 import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 
 import 'app.dart';
@@ -6,5 +8,11 @@ import 'app.dart';
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp();
+  await FirebaseAppCheck.instance.activate(
+    androidProvider: kDebugMode ? AndroidProvider.debug : AndroidProvider.playIntegrity,
+    appleProvider: kDebugMode
+        ? AppleProvider.debug
+        : AppleProvider.appAttestWithDeviceCheckFallback,
+  );
   runApp(const PolycircleApp());
 }
