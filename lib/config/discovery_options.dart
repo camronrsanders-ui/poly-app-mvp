1e74c3ca7cc89b0b06dcfaa6bb9c4c38d521a998
camronrsanders-ui <camron.r.sanders@gmail.com>
2026-08-08 23:06:12 -0400

    Centralize relationship and intention options

---
+++ lib/config/discovery_options.dart
@@ -0,0 +1,22 @@
+const relationshipStructureOptions = <String>[
+  'Solo poly',
+  'Hierarchical poly',
+  'Non-hierarchical poly',
+  'Open relationship',
+  'Polyfidelity',
+  'Relationship anarchy',
+  'Monogamish',
+  'Exploring',
+  'Custom / self-described',
+];
+
+const connectionIntentionOptions = <String>[
+  'Friendship',
+  'Community',
+  'Dating',
+  'Long-term relationship',
+  'Casual connection',
+  'Join a polycule',
+  'Build / grow a polycule',
+  'Exploring / learning',
+];
