5a4213716589dc347ab05386a310f502a43346d9
camronrsanders-ui <camron.r.sanders@gmail.com>
2026-08-08 20:53:10 -0400

    Remove unsafe unscoped match stream

--- lib/services/connection_service.dart
+++ lib/services/connection_service.dart
@@ -1,17 +1,13 @@
-import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:cloud_functions/cloud_functions.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 
 class ConnectionService {
   ConnectionService({
-    FirebaseFirestore? firestore,
     FirebaseAuth? auth,
     FirebaseFunctions? functions,
-  })  : _firestore = firestore ?? FirebaseFirestore.instance,
-        _auth = auth ?? FirebaseAuth.instance,
+  })  : _auth = auth ?? FirebaseAuth.instance,
         _functions = functions ?? FirebaseFunctions.instance;
 
-  final FirebaseFirestore _firestore;
   final FirebaseAuth _auth;
   final FirebaseFunctions _functions;
 
@@ -23,14 +19,5 @@ class ConnectionService {
     final callable = _functions.httpsCallable('likeProfile');
     final result = await callable.call<Map<String, dynamic>>({'toUid': toUid});
     return result.data['matched'] == true;
-  }
-
-  Stream<QuerySnapshot<Map<String, dynamic>>> watchMatches() {
-    final uid = _auth.currentUser?.uid;
-    if (uid == null) return const Stream.empty();
-    return _firestore
-        .collection('matches')
-        .where('active', isEqualTo: true)
-        .snapshots();
   }
 }
