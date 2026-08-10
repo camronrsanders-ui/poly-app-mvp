b197e886f99d3fa3d1d7a87ec8a66f66f4753c4f
camronrsanders-ui <camron.r.sanders@gmail.com>
2026-08-10 08:49:58 -0400

    Wire opt-in Firebase emulators into debug startup

--- lib/main.dart
+++ lib/main.dart
@@ -4,10 +4,12 @@ import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 
 import 'app.dart';
+import 'config/firebase_runtime.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp();
+  await configureFirebaseRuntime();
   await FirebaseAppCheck.instance.activate(
     providerAndroid: kDebugMode
         ? const AndroidDebugProvider()
