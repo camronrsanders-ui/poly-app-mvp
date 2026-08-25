677e25db48c4c6934befc3dbcbc2e5a7df80b395
Camron <camron.r.sanders@gmail.com>
2026-08-25 02:53:15 -0400

    feat: add privacy-minimal crash reporting

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,47 @@
+import 'dart:async';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_app_check/firebase_app_check.dart';
 import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_crashlytics/firebase_crashlytics.dart';
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 
 import 'app.dart';
 import 'config/firebase_runtime.dart';
+
+Future<void> _configureCrashReporting() async {
+  final crashReportingEnabled =
+      !kIsWeb && kReleaseMode && !useFirebaseEmulators && !localReleaseSmoke;
+
+  if (kIsWeb) {
+    return;
+  }
+
+  await FirebaseCrashlytics.instance.setCrashlyticsCollectionEnabled(
+    crashReportingEnabled,
+  );
+
+  if (!crashReportingEnabled) {
+    return;
+  }
+
+  FlutterError.onError = (details) {
+    unawaited(
+      FirebaseCrashlytics.instance.recordFlutterFatalError(details),
+    );
+  };
+
+  PlatformDispatcher.instance.onError = (error, stack) {
+    unawaited(
+      FirebaseCrashlytics.instance.recordError(
+        error,
+        stack,
+        fatal: true,
+      ),
+    );
+    return true;
+  };
+}
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -22,6 +58,7 @@ Future<void> main() async {
   }
 
   await configureFirebaseRuntime();
+  await _configureCrashReporting();
   final localSmokeAppCheck = localReleaseSmoke && useFirebaseEmulators;
 
   await FirebaseAppCheck.instance.activate(
